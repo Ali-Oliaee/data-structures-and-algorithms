aa17f94cb749bc1720ba4b7e82a84ab4e132a75c
james <ali_o@felixin.io>
2023-01-03 00:37:48 +0330

    create home page

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,22 @@
 import React from "react"
 import ReactDOM from "react-dom/client"
-import App from "./App"
+import { createBrowserRouter, RouterProvider } from "react-router-dom"
+import { BST, HomePage } from "./pages"
 import "./index.scss"
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <HomePage />,
+  },
+  {
+    path: "/bst",
+    element: <BST />,
+  },
+])
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 )
