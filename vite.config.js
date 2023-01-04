c69a0e315ed5dacc56dc84e0060748daf6f3b132
james <ali_o@felixin.io>
2023-01-04 13:05:54 +0330

    change base

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react"
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "https://ali-oliaee.github.io/data-structures-qiet/",
+  base: "/data-structures-qiet/",
 })
