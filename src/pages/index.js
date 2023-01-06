1196a4abf4857beaec38357faf265db3beac658e
james <ali_o@felixin.io>
2023-01-06 12:41:23 +0330

    create stack page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,4 @@
 export { default as BST } from "./BST"
 export { default as HomePage } from "./Home"
 export { default as ArrayPage } from "./Array"
+export { default as StackPage } from "./Stack"
--- src/router/index.jsx
+++ src/router/index.jsx
@@ -1,5 +1,5 @@
 import { createBrowserRouter } from "react-router-dom"
-import { BST, HomePage, ArrayPage } from "../pages"
+import { BST, HomePage, ArrayPage, StackPage } from "../pages"
 
 const router = createBrowserRouter([
   {
@@ -13,6 +13,10 @@ const router = createBrowserRouter([
   {
     path: "/data-structures-qiet/bst",
     element: <BST />,
+  },
+  {
+    path: "/data-structures-qiet/stack",
+    element: <StackPage />,
   },
 ])
 
