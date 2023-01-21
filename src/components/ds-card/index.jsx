75022137c0e04f0e8fdf4d8d44e7090b9c023e82
james <ali_o@felixin.io>
2023-01-21 20:51:56 +0330

    add styles to home page

---
+++ src/components/ds-card/index.jsx
@@ -0,0 +1,21 @@
+import React from "react"
+import { Link } from "react-router-dom"
+import "./styles.scss"
+
+const DatastructureCard = ({ image, title, description, link }) => {
+  return (
+    <div className="ds-card">
+      <Link to={link}>
+        <div className="card-image">
+          <img src={image} alt={title} />
+        </div>
+        <div className="card-content">
+          <h3 className="card-title">{title}</h3>
+          <p className="card-description">{description}</p>
+        </div>
+      </Link>
+    </div>
+  )
+}
+
+export default DatastructureCard
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,24 +1,98 @@
-import { Link } from "react-router-dom"
+import { DatastructureCard } from "@/components"
+import array from "@assets/array.png"
+import stack from "@assets/stack.png"
+import queue from "@assets/queue.png"
+import bst from "@assets/bst.png"
+import sort from "@assets/sort.png"
+import postfix from "@assets/postfix-expression.png"
+import ext from "@assets/ext.png"
+import maxHeap from "@assets/max-heap.png"
+import minHeap from "@assets/min-heap.png"
+import "./styles.scss"
 
 const HomePage = () => {
+  const customInit = async (engine) => await loadFireflyPreset(engine)
+
   return (
-    <div>
-      <h1>Home Page</h1>
-      <Link to="/data-structures-qiet/array">Array</Link>
-      <br />
-      <Link to="/data-structures-qiet/stack">Stack</Link>
-      <br />
-      <Link to="/data-structures-qiet/queue">Queue</Link>
-      <br />
-      <Link to="/data-structures-qiet/bst">BST</Link>
-      <br />
-      <Link to="/data-structures-qiet/ext">EXT</Link>
-      <br />
-      <Link to="/data-structures-qiet/expressions">Expressions</Link>
-      <br />
-      <Link to="/data-structures-qiet/min-heap">Min Heap</Link>
-      <br />
-      <Link to="/data-structures-qiet/max-heap">Max Heap</Link>
+    <div className="home-page">
+      <h1 className="title">Welcome</h1>
+      <h4 className="divider-text">to</h4>
+      <h2 className="subtitle">Data Structures</h2>
+      <div className="card-container">
+        <DatastructureCard
+          title="Sorting Algorithms"
+          description="Sorting is the process of arranging a collection of items in a
+          particular order. Sorting algorithms are used to arrange data in a
+          particular format."
+          image={sort}
+          // link="/sorting-algorithms-qiet"
+        />
+        <DatastructureCard
+          title="Array"
+          description=" An array is a collection of elements of the same type placed in
+          contiguous memory locations that can be individually referenced by
+          using an index to a unique identifier."
+          image={array}
+          link="/data-structures-qiet/array"
+        />
+        <DatastructureCard
+          title="Stack"
+          description="Stacks in Data Structures is a linear type of data structure that
+            follows the LIFO (Last-In-First-Out)"
+          image={stack}
+          link="/data-structures-qiet/stack"
+        />
+
+        <DatastructureCard
+          title="Queue"
+          description="A queue is a linear structure which follows a particular order in
+          which the operations are performed. The order is First In First Out 
+          (FIFO)."
+          image={queue}
+          link="/data-structures-qiet/queue"
+        />
+
+        <DatastructureCard
+          title="Binary Search Tree"
+          description="A binary search tree is a binary tree in which the value of each
+          internal node is greater than or equal to all the values in the nodes
+          in that node's left subtree and less than or equal to those in its
+          right subtree."
+          image={bst}
+          link="/data-structures-qiet/bst"
+        />
+        <DatastructureCard
+          title="Expressions"
+          description="An expression is a combination of one or more constants, variables,
+          operators, and the function calls that evaluates to a single value."
+          image={postfix}
+          link="/data-structures-qiet/expressions"
+        />
+        <DatastructureCard
+          title="Expression Tree"
+          description="An expression tree is a binary tree in which each internal node
+          corresponds to an operator and each leaf node corresponds to an
+          operand."
+          image={ext}
+          link="/data-structures-qiet/ext"
+        />
+        <DatastructureCard
+          title="Min Heap"
+          description="A min heap is a complete binary tree in which the value in each
+          internal node is smaller than or equal to the values in the children
+          of that node."
+          image={minHeap}
+          link="/data-structures-qiet/min-heap"
+        />
+        <DatastructureCard
+          title="Max Heap"
+          description="A max heap is a complete binary tree in which the value in each
+          internal node is greater than or equal to the values in the children
+          of that node."
+          image={maxHeap}
+          link="/data-structures-qiet/max-heap"
+        />
+      </div>
     </div>
   )
 }
