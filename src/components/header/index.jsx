8b7203e4258c3b87a1394903759f50f8607e99a7
Ali <alioliaee31@gmail.com>
2023-02-27 12:46:51 +0330

    improve header

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -1,5 +1,10 @@
 import { Layout } from "antd"
-import { GithubFilled } from "@ant-design/icons"
+import {
+  GithubFilled,
+  InstagramFilled,
+  LinkedinFilled,
+  LinkOutlined,
+} from "@ant-design/icons"
 import { Link } from "react-router-dom"
 import "./styles.scss"
 
@@ -8,14 +13,35 @@ const Header = () => {
 
   return (
     <Header className="header">
-      <Link to="/data-structures-qiet/">Home</Link>
-      <a
-        target="_blank"
-        href="https://github.com/ali-oliaee/"
-        className="github-link"
-      >
-        <GithubFilled className="github-icon" />
-      </a>
+      <Link to="/data-structures-qiet/" className="home-link">
+        Home
+      </Link>
+      <div className="links">
+        <a
+          target="_blank"
+          href="https://github.com/ali-oliaee/"
+          className="link"
+        >
+          <GithubFilled className="icon" />
+        </a>
+        <a
+          target="_blank"
+          href="https://linkedinin.com/in/ali-oliaee/"
+          className="link"
+        >
+          <LinkedinFilled className="icon" />
+        </a>
+        <a
+          target="_blank"
+          href="https://instagram.com/ali._.oliaee/"
+          className="link"
+        >
+          <InstagramFilled className="icon" />
+        </a>
+        <a target="_blank" href="https://ali-oliaee.ir" className="link">
+          <LinkOutlined className="icon" />
+        </a>
+      </div>
     </Header>
   )
 }
