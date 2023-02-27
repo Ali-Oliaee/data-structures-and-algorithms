7baf71e89571eaab3ce7e3415d96c1f4a27fd54f
Ali <alioliaee31@gmail.com>
2023-02-27 12:46:46 +0330

    improve queue page

--- src/pages/queue/index.jsx
+++ src/pages/queue/index.jsx
@@ -1,19 +1,26 @@
 import { useState } from "react"
 import MainLayout from "@layouts"
-import { Button, Collapse } from "antd"
+import { Button, Collapse, message } from "antd"
 import Codes from "./codes"
 import "./styles.scss"
 
 const QueuePage = () => {
+  const [messageApi, contextHolder] = message.useMessage()
   const { Panel } = Collapse
   const [queue, setQueue] = useState([1, 3])
-  const dequeue = () => setQueue(queue.slice(1))
+  const dequeue = () => {
+    if (queue.length === 0) return messageApi.error("Queue is empty")
+    setQueue(queue.slice(1))
+  }
   const enqueue = () => {
+    if (queue.length === 12) return messageApi.error("Queue is full")
     const random = Math.floor(Math.random() * 100)
     setQueue([...queue, random])
   }
+
   return (
     <MainLayout>
+      {contextHolder}
       <div className="queue-page">
         <h4>Queue</h4>
         <p>
@@ -26,14 +33,18 @@ const QueuePage = () => {
           <div className="queue">
             {queue.map((item, index) => (
               <span key={index} className="queue-item">
+                {queue.length === 1 && (
+                  <span className="queue-item-last top-queue">Tail</span>
+                )}
                 {item}
-                {index === queue.length - 1 && (
+                {index === queue.length - 1 && queue.length !== 1 && (
                   <span className="queue-item-last">Tail</span>
                 )}
                 {index === 0 && <span className="queue-item-first">Head</span>}
               </span>
             ))}
           </div>
+          <p className="size-text">Queue size: 12</p>
           <div className="buttons">
             <Button className="button" type="primary" onClick={enqueue}>
               Enqueue
